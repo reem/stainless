062fc1d5fb9bbb229474370c82d94902e90c8880
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-01 18:45:25 -0700

    (feat) describe! parser.

--- src/describe.rs
+++ src/describe.rs
@@ -27,3 +27,72 @@ struct Test {
     block: P<ast::Block>
 }
 
+fn parse_describe(mut state: DescribeState, mut parser: parse::parser::Parser) -> DescribeState {
+    // First parse the name of this describe block:
+    let (name, _) = parser.parse_str();
+    state.name = Some(name.get().to_string());
+
+    // Move past the opening {
+    if parse::token::LBRACE != parser.bump_and_get() {
+        parser.fatal("Expected { after the description of a describe! block.");
+    }
+
+    // Now parse all tests and subsections:
+    while parser.token != parse::token::RBRACE {
+        // Get the name of this block, must be either:
+        //     - before_each
+        //     - after_each
+        //     - before
+        //     - after
+        //     - it
+        //
+        // Any other top-level idents are not allowed.
+        let block_name = parser.parse_ident();
+
+        match block_name.as_str() {
+            BEFORE_EACH => {
+                if state.before_each.is_some() { parser.fatal("Only one `before_each` block is allowed per `describe!` block.") }
+                state.before_each = Some(parser.parse_block());
+            },
+
+            AFTER_EACH => {
+                if state.after_each.is_some() { parser.fatal("Only one `after_each` block is allowed per `describe!` block.") }
+                state.after_each = Some(parser.parse_block());
+            },
+
+            BEFORE => {
+                if state.before.is_some() { parser.fatal("Only one `before` block is allowed per `describe!` block.") }
+                state.before = Some(parser.parse_block());
+            },
+
+            AFTER => {
+                if state.after.is_some() { parser.fatal("Only one `after` block is allowed per `describe!` block.") }
+                state.after = Some(parser.parse_block());
+            },
+
+            IT => {
+                // Description of this `it` block.
+                let (description, _) = parser.parse_str();
+
+                state.tests.push(Test {
+                    // Get as a String
+                    description: description.get().to_string(),
+
+                    // The associated block
+                    block: parser.parse_block()
+                });
+            },
+
+            banned => {
+                // Illegal block name.
+                let span = parser.span;
+                parser.span_fatal(span, format!("Expected one of: `{}`, but found: `{}`",
+                    format!("{}, {}, {}, {}, {}", BEFORE_EACH, AFTER_EACH, BEFORE, AFTER, IT).as_slice(),
+                    banned).as_slice());
+            }
+        }
+    }
+
+    state
+}
+
