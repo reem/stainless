cfb4aa23f04a250e5721548aba057668b6a44dfb
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-01 18:59:32 -0700

    (test) Added after_each to tests.

--- tests/simple.rs
+++ tests/simple.rs
@@ -10,5 +10,10 @@ describe!("addition" {
     it "should add 5 and 6 together" {
         assert_eq!(x + y, 11);
     }
+
+    after_each {
+        assert_eq!(x, 5);
+        assert_eq!(y, 6);
+    }
 })
 
