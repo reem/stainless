73cf3f4c5422c4618cec1352ddddf373ff2c0d68
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-01 22:39:45 -0700

    (chore) Deny missing_doc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![license = "MIT"]
 #![feature(plugin_registrar)]
 #![deny(warnings)]
+#![deny(missing_doc)]
 
 //! Stainless is a lightweight, unopinionated testing framework for Rust.
 
@@ -13,6 +14,7 @@ use rustc::plugin;
 mod describe;
 
 #[plugin_registrar]
+#[doc(hidden)]
 pub fn plugin_registrar(reg: &mut plugin::Registry) {
     reg.register_macro("describe", describe);
 }
