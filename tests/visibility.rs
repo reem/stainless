e28884913fd437b4809b0399dc042658abce5cd2
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-01 20:37:01 -0700

    (test) Test that `it` tests can see outer pub items.

---
+++ tests/visibility.rs
@@ -0,0 +1,17 @@
+#![feature(phase)]
+#[phase(plugin, link)]
+extern crate stainless;
+
+pub struct X(int);
+
+#[cfg(test)]
+mod test {
+    // This use must be pub so that the addition sub-module can view it.
+    pub use super::X;
+
+    describe!("stainless" {
+        it "should be able to see outer pub uses" {
+            let _ = X(5);
+        }
+    })
+}
