cef7727f743bd9d2b63d219953edad97ec18af9b
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-04 03:52:35 -0700

    (test) Added test and example for using helpers.

---
+++ tests/helpers.rs
@@ -0,0 +1,17 @@
+#![feature(phase)]
+#[phase(plugin, link)]
+extern crate stainless;
+
+#[cfg(test)]
+mod test {
+    pub fn test_helper<T: PartialEq>(x: T, y: T) {
+        if x != y { fail!("Not equal.") }
+    }
+
+    describe! helpers {
+        it "should be able to use helpers" {
+            test_helper(7u, 7);
+        }
+    }
+}
+
