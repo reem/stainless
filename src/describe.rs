fe9e4e6fc0f1ad1b02d83f2ae3e645cddfbcebbf
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-06 23:02:35 -0700

    (doc) Added explanation of internal code.

--- src/describe.rs
+++ src/describe.rs
@@ -1,3 +1,27 @@
+/// ## Internal Code Guide
+///
+/// This crate is centered around two traits, Parse and Generate,
+/// which define how to parse a struct from a Parser over Rust code
+/// and how to generate Rust code from a struct respectively.
+///
+/// All the parsing and generation of code is done through these traits.
+///
+/// `describe` is responsible for expanding the `describe!` macro
+/// as a whole, and delegates to the Parse and Generate implementations
+/// for `DescribeState`, which holds all the information necessary to
+/// generate the AST for an expanded `describe!`.
+///
+/// The Parse implementation of DescribeState delegates to the Parse
+/// implementation of Test, defined in the test module, and Bench,
+/// defined in the bench module. The Generate implementation does the
+/// same.
+///
+/// Most of the code can be understood by just walking through the
+/// implementations of Parse and Generate for all the types inside
+/// this crate, which can be found in the parse and generate
+/// modules respectively.
+///
+
 use syntax::{ast, codemap, parse};
 use syntax::ptr::P;
 use syntax::ext::base;
@@ -8,6 +32,7 @@ use test::Test;
 use bench::Bench;
 
 /// Defines the state of a `describe!` macro as it is parsing.
+#[deriving(Clone)]
 pub struct DescribeState {
     pub name: Option<ast::Ident>,
     pub before: Option<P<ast::Block>>,
@@ -22,7 +47,7 @@ pub struct DescribeState {
 pub enum SubBlock {
     TestBlock(Test),
     BenchBlock(Bench),
-    DescribeBlock(P<ast::Item>)
+    DescribeBlock(DescribeState)
 }
 
 /// Defines the overarching `describe!` syntax extension.
